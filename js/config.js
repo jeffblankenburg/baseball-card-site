1308fb5dd6b15fd8712022c57008268c0e49192d
Jeff Blankenburg <github@jeffblankenburg.com>
2021-02-17 16:09:21 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_Rk9Q6RwAq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1l8knbqmq9p8kbgdd3b0l10ire', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
